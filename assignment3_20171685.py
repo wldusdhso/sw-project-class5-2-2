86d94c482d1bd31487b86aa9202975422719a571
Lhyejin <hub0913@kookmin.ac.kr>
2017-09-13 19:59:14 +0900

    Add files via upload

---
+++ assignment3_20171685.py
@@ -0,0 +1,76 @@
+dbfilename = 'test3_3.dat'
+
+def readScoreDB():
+	try:
+		fH = open(dbfilename)
+	except FileNotFoundError as e:
+		print("New DB: ", dbfilename)
+		return []
+	else:
+		print("Open DB: ", dbfilename)
+
+	line = fH.read()
+	scdb = eval(line)
+	fH.close()
+	return scdb
+
+
+# write the data into person db
+def writeScoreDB(scdb):
+	fH = open(dbfilename, 'w')
+	fH.write(str(scdb) + '\n')
+	fH.close()
+
+def doScoreDB(scdb):
+	while(True):
+		inputstr = (input("Score DB > "))
+		if inputstr == "": continue
+		parse = inputstr.split(" ")
+
+		try :
+			if parse[0] == 'add':
+				record = {'Name':parse[1], 'Age':parse[2], 'Score':parse[3]}
+				scdb += [record]
+			elif parse[0] == 'del':
+				for p in scdb:
+					if p['Name'] == parse[1]:
+						scdb.remove(p)
+			elif parse[0] == 'show':
+				sortKey ='Name' if len(parse) == 1 else parse[1]
+				showScoreDB(scdb, sortKey)
+			elif parse[0] == 'find':#find data
+				finddb=[]
+				for p in scdb:
+					if p['Name'] == parse[1]:
+						finddb +=[p]
+						sortKey ='Name'
+						showScoreDB(finddb, sortKey)
+			elif parse[0] == 'inc':#increse score
+				for p in scdb:
+					if p['Name'] == parse[1]:
+						p['Score'] = str(int(p['Score'])+int(parse[2]))
+			elif parse[0] == 'quit':
+				break
+			else:
+				print("Invalid command: " + parse[0])
+		except IndexError as e:
+			print("Index Error")
+
+		else: continue
+
+
+
+
+
+def showScoreDB(scdb, keyname):
+	for p in sorted(scdb, key=lambda person: person[keyname]):
+		for attr in sorted(p):
+			print(attr + "=" + p[attr], end=' ')
+		print()
+
+
+
+scoredb = readScoreDB()
+doScoreDB(scoredb)
+writeScoreDB(scoredb)
+
