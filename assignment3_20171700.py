aa357e729c9be8204838576e870017d84cb1073c
jung211p <jung211p@naver.com>
2017-09-14 03:11:06 +0900

    Add files via upload

---
+++ assignment3_20171700.py
@@ -0,0 +1,91 @@
+import pickle
+
+dbfilename = 'test3_4.dat'
+
+
+def readScoreDB():
+    try:
+        fH = open(dbfilename, 'rb')
+    except FileNotFoundError as e:
+        print("New DB: ", dbfilename)
+        return []
+
+    scdb = []
+    try:
+        scdb = pickle.load(fH)
+    except:
+        print("Empty DB: ", dbfilename)
+    else:
+        print("Open DB: ", dbfilename)
+    fH.close()
+    return scdb
+
+
+# write the data into person db
+def writeScoreDB(scdb):
+    fH = open(dbfilename, 'wb')
+    pickle.dump(scdb, fH)
+    fH.close()
+
+
+def doScoreDB(scdb):
+    while (True):
+        inputstr = (input("Score DB > "))
+        if inputstr == " ": continue
+        parse = inputstr.split(" ")
+        if parse[0] == 'add':
+            try:
+                record = {'Name': parse[1], 'Age': parse[2], 'Score': parse[3]}
+                scdb += [record]
+            except:
+                continue
+
+        elif parse[0] == 'find':
+            for p in scdb:
+                if p['Name'] == parse[1]:
+                    try:
+                        sortKey = 'Name'
+                        p.find('Name')
+                        showScoreDB(find, sortKey)
+                    except:
+                        continue
+                            
+        elif parse[0] == 'del':
+            for p in scdb:
+                if p['Name'] == parse[1]:
+                    try:
+                        scdb.remove(p)
+                    except:
+                        continue
+
+        elif parse[0] == 'inc':
+            for p in scdb:
+                if p['Name'] == parse[1]:
+                    try:
+                        p['Score'] = str(int(p['Score']) + int(parse[3]))
+                    except:
+                        continue
+
+        elif parse[0] == 'show':
+            sortKey = 'Name' if len(parse) == 1 else parse[1]
+            showScoreDB(scdb, sortKey)
+
+        elif parse[0] == 'quit':
+            break
+
+        else:
+            print("Invalid command: " + parse[0])
+
+
+
+
+def showScoreDB(scdb, keyname):
+    for p in sorted(scdb, key=lambda person: person[keyname]):
+        for attr in sorted(p):
+            print(attr + "=" + p[attr], end=' ')
+        print()
+
+
+scoredb = readScoreDB()
+doScoreDB(scoredb)
+writeScoreDB(scoredb)
