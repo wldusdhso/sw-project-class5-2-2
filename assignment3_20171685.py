aa433b6a97868e1f91aa0bd8678a518097e0627c
Lhyejin <hub0913@kookmin.ac.kr>
2017-09-13 20:15:33 +0900

    Update assignment3_20171685.py

--- assignment3_20171685.py
+++ assignment3_20171685.py
@@ -1,32 +1,36 @@
-dbfilename = 'test3_3.dat'
+import pickle
+
+dbfilename = 'test3_4.dat'
 
 def readScoreDB():
 	try:
-		fH = open(dbfilename)
+		fH = open(dbfilename, 'rb')
 	except FileNotFoundError as e:
 		print("New DB: ", dbfilename)
 		return []
+
+	scdb = []
+	try:
+		scdb =  pickle.load(fH)
+	except:
+		print("Empty DB: ", dbfilename)
 	else:
 		print("Open DB: ", dbfilename)
-
-	line = fH.read()
-	scdb = eval(line)
 	fH.close()
 	return scdb
 
 
 # write the data into person db
 def writeScoreDB(scdb):
-	fH = open(dbfilename, 'w')
-	fH.write(str(scdb) + '\n')
-	fH.close()
+    fH = open(dbfilename, 'wb')
+    pickle.dump(scdb, fH)
+    fH.close()
 
 def doScoreDB(scdb):
 	while(True):
 		inputstr = (input("Score DB > "))
 		if inputstr == "": continue
 		parse = inputstr.split(" ")
-
 		try :
 			if parse[0] == 'add':
 				record = {'Name':parse[1], 'Age':parse[2], 'Score':parse[3]}
@@ -58,19 +62,14 @@ def doScoreDB(scdb):
 
 		else: continue
 
-
-
-
-
 def showScoreDB(scdb, keyname):
 	for p in sorted(scdb, key=lambda person: person[keyname]):
 		for attr in sorted(p):
 			print(attr + "=" + p[attr], end=' ')
 		print()
-
+	
 
 
 scoredb = readScoreDB()
 doScoreDB(scoredb)
 writeScoreDB(scoredb)
-
