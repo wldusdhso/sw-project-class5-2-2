595c89fa6d5c9049dbf2bcb1809890083301e5d3
jung211p <jung211p@naver.com>
2017-09-26 10:13:26 +0900

    Add files via upload

--- assignment4_20171700.py
+++ assignment4_20171700.py
@@ -1,11 +1,19 @@
-n = int(input("Enter number(n) : "))
-r = int(input("Enter number(r) : "))
+n = 1
+r = 1
 def nCr(n, r):
     if r == 0 or n== r:
         return 1
     elif n < r :
         print("Try again")
     else:
-        return int(nCr(n-1, r-1) + nCr(n-1, r))
+        return nCr(n-1, r-1) + nCr(n-1, r)
 
-print(nCr(n, r))
+while True:
+    try:
+        if r>=0 and n >= 0:
+            n = int(input("Enter number(n) : "))
+            r = int(input("Enter number(r) : "))
+            print(nCr(n, r))
+
+    except:
+        break
