0c5794426981ec1e8bc948a8f7c41c2ba725789b
Lhyejin <hub0913@kookmin.ac.kr>
2017-09-26 09:05:28 +0900

    Add files via upload

---
+++ assignment4_201717685.py
@@ -0,0 +1,22 @@
+
+def CombiNation(n, m):
+    if m == 1:
+        return n
+    return CombiNation(n - 1, m - 1) * (n/m)
+
+fstnum = 1
+while fstnum > 0 and secnum > 0:
+
+    fstnum = int(input("Enter n: "))
+    secnum = int(input("Enter m: "))
+
+    print("C(%d,%d) =" %(fstnum,secnum),CombiNation(fstnum,secnum))
+
+
+
+
+
+
+
+
+
