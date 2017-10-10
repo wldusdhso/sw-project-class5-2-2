ccd0674c02cf24078db8be7e1eba912d72c810d2
wldusdhso <wldusdhso@kookmin.ac.kr>
2017-10-10 09:44:41 +0900

    Add files via upload

--- assignment5_20171699.py
+++ assignment5_20171699.py
@@ -6,18 +6,18 @@ def fibo(n):
     return fibo(n - 1) + fibo(n - 2)
 
 def iterfibo(n):
-   num =1
-   a = 0
-   if n ==0:
-       return 0
-   elif n == 1:
-       return 1
-   else:
-       for i in range(n-1):
-           sum = num + a
-           a = num
-           num = sum
-       return sum
+    num=1
+    re = 0
+    if n==0:
+        return 0
+    elif n ==1:
+        return 1
+    else:
+        for i in range(n-1):
+            sum = re+ num
+            re = num
+            num = sum
+    return sum
 
 while True:
     nbr = int(input("Enter a number: "))
@@ -31,4 +31,8 @@ while True:
     fibonumber = fibo(nbr)
     ts = time.time() - ts
     print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+
+
+
+
 
