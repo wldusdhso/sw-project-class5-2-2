3511779056895fb217181a174777b6943d313564
jung211p <jung211p@naver.com>
2017-11-14 09:08:12 +0900

    Add files via upload

--- keypad2.py
+++ keypad2.py
@@ -1,4 +1,3 @@
-import calcFunctions
 
 numPadList = [
     '7', '8', '9',
@@ -14,16 +13,17 @@ operatorList = [
     'C',
 ]
 
-constantList = {
-    'pi': '3.141592',
+constantDic = {
+    'pi':'3.141592',
     '빛의 이동 속도 (m/s)':'3E+8',
     '소리의 이동 속도 (m/s)':'340',
     '태양과의 평균 거리 (km)':'1.5E+8',
 }
 
-functionList = {
-    'factorial (!)': calcFunctions.factorial,
-    '-> binary': calcFunctions.decToBin,
-    'binary -> dec': calcFunctions.binToDec,
-    '-> roman': calcFunctions.decToRoman,
-}
+
+functionList =[
+    'factorial (!)' ,
+    '-> binary',
+    'binary -> dec',
+    '-> roman',
+]
